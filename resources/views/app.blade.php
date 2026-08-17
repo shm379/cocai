3a0f028ed19581c8bd03112451d00e65d215aa32
Hussein <shm379@live.com>
2026-08-17 14:15:22 +0400

    style: apply Vazirmatn font and game theme styling across all pages

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,10 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- Vazirmatn Persian Gaming Font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100..900&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @routes
